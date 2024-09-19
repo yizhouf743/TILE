ecb2b7705a4911b492866f9b8cead8c2b2a04fcb
feng002 <59892828+feng002@users.noreply.github.com>
2024-09-19 00:02:36 -0400

    Update throttle.sh

--- throttle.sh
+++ throttle.sh
@@ -9,24 +9,18 @@ fi
 if [ "$1" == "lan" ]
 then
 sudo tc qdisc del dev $DEV root
-# about 3Gbps
 sudo tc qdisc add dev $DEV root handle 1: tbf rate 3000mbit burst 100000 limit 10000
-# about 0.3ms ping latency
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 0.15msec
 fi
 if [ "$1" == "wan" ]
 then
 sudo tc qdisc del dev $DEV root
-# about 400Mbps
 sudo tc qdisc add dev $DEV root handle 1: tbf rate 400mbit burst 100000 limit 10000
-# about 40ms ping latency
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 20msec
 fi
 if [ "$1" == "mobile_US_avg" ]
 then
 sudo tc qdisc del dev $DEV root
-# about 125Mbps
 sudo tc qdisc add dev $DEV root handle 1: tbf rate 125mbit burst 100000 limit 10000
-# about 30ms ping latency
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 15msec
 fi
