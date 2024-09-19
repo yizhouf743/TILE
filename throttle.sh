14b6f0cf4ca90f815bf7399a3c265e964953ccf6
feng002 <59892828+feng002@users.noreply.github.com>
2024-09-18 23:48:01 -0400

    Add files via upload

---
+++ throttle.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Source code is available from https://github.com/emp-toolkit/emp-readme/blob/master/scripts/throttle.sh
+DEV=lo 
+if [ "$1" == "del" ]
+then
+	sudo tc qdisc del dev $DEV root
+fi
+
+if [ "$1" == "lan" ]
+then
+sudo tc qdisc del dev $DEV root
+## about 3Gbps
+sudo tc qdisc add dev $DEV root handle 1: tbf rate 3000mbit burst 100000 limit 10000
+## about 0.3ms ping latency
+sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 0.15msec
+fi
+if [ "$1" == "wan" ]
+then
+sudo tc qdisc del dev $DEV root
+## about 400Mbps
+sudo tc qdisc add dev $DEV root handle 1: tbf rate 400mbit burst 100000 limit 10000
+## about 40ms ping latency
+sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 20msec
+fi
+if [ "$1" == "mobile_US_avg" ]
+then
+sudo tc qdisc del dev $DEV root
+## about 125Mbps
+sudo tc qdisc add dev $DEV root handle 1: tbf rate 125mbit burst 100000 limit 10000
+## about 30ms ping latency
+sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 15msec
+fi
