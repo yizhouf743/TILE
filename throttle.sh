55da80ae04550df9e67b86909ed597f7d400725a
feng002 <59892828+feng002@users.noreply.github.com>
2024-10-05 14:03:27 -0400

    Update throttle.sh

--- throttle.sh
+++ throttle.sh
@@ -18,6 +18,7 @@ sudo tc qdisc del dev $DEV root
 sudo tc qdisc add dev $DEV root handle 1: tbf rate 400mbit burst 100000 limit 10000
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 20msec
 fi
+# This test environment based on the real-world network performance data reported on https://www.speedtest.net/global-index/united-states.
 if [ "$1" == "mobile_US_avg" ]
 then
 sudo tc qdisc del dev $DEV root
