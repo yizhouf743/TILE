8d328f0940e398050e80e909db10850aab509961
yizhou feng <59892828+yizhouf743@users.noreply.github.com>
2024-10-17 00:07:55 -0400

    Update throttle.sh

--- throttle.sh
+++ throttle.sh
@@ -6,6 +6,8 @@ then
 	sudo tc qdisc del dev $DEV root
 fi
 
+# This LAN and WAN network configuration based on the setting in Cryptflow2 and Cheetah, 
+# which is available from https://github.com/Alibaba-Gemini-Lab/OpenCheetah/tree/main/scripts.
 if [ "$1" == "lan" ]
 then
 sudo tc qdisc del dev $DEV root
@@ -18,7 +20,7 @@ sudo tc qdisc del dev $DEV root
 sudo tc qdisc add dev $DEV root handle 1: tbf rate 400mbit burst 100000 limit 10000
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 20msec
 fi
-# This test environment based on the real-world network performance data reported on Ookla dataset, 
+# This mobile_US_avg network configuration based on the real-world network performance data reported on Ookla dataset, 
 # which is available from https://www.speedtest.net/global-index/united-states.
 if [ "$1" == "mobile_US_avg" ]
 then
