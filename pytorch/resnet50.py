452303d0cad300c99983c9d07d16113de1864f7a
yizhou feng <59892828+yizhouf743@users.noreply.github.com>
2024-10-29 18:12:04 -0400

    Update resnet50.py

--- pytorch/resnet50.py
+++ pytorch/resnet50.py
@@ -273,13 +273,13 @@ def cal_partial_order(replace_order, search_range, cn=[2, 2, 2, 2], threshold=2)
       # rs = sr * rs * sf
       rs = rs * sf
   
-      layer_order = layer_order.view(-1, cpc)
-      layer_order = layer_order.sum(dim=1).repeat_interleave(cpc)
+      # layer_order = layer_order.view(-1, cpc)
+      # layer_order = layer_order.sum(dim=1).repeat_interleave(cpc)
       if 'conv2' in name:
         indices = torch.where(layer_order < threshold)[0]
       else:
-        # layer_order = layer_order.view(-1, cpc)
-        # layer_order = layer_order.sum(dim=1).repeat_interleave(cpc)
+        layer_order = layer_order.view(-1, cpc)
+        layer_order = layer_order.sum(dim=1).repeat_interleave(cpc)
         if threshold > 0:
           indices = torch.where(layer_order < (threshold * rs))[0]
         else:
